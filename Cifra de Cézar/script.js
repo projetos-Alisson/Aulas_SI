1041882e999cadf413cb8cd4f3675d5824265855
Alisson Soares <79875609+projetos-Alisson@users.noreply.github.com>
2023-06-14 20:49:32 -0300

    Add files via upload

--- Cifra de Cézar/script.js
+++ Cifra de Cézar/script.js
@@ -1,22 +1,51 @@
 function encryptMensagem() {
-    var mensagem = document.getElementById('mensagem');
-    var encrypt = document.getElementById('encrypted');
+    let mensagem = document.getElementById('mensagem');
+    let encrypt = document.getElementById('encrypted');
+
+    console.log(mensagem.value)
 
     mensagem = mensagem.value.toUpperCase().replace(/\s/g,'');
 
-    for(var a = 0; a < mensagem.length; a++) {
-        var letterCode;
-        var offset;
+    for(let a = 0; a < mensagem.length; a++) {
+        let letterCode;
+        let offset;
 
         letterCode = mensagem.charCodeAt(a) - 65;
         offset = (letterCode + 3) % 26;
        
         offset += 65;
 
-        encrypt.value += String.fromCharCode(offset)}
+        encrypt.value += String.fromCharCode(offset)
+         }
+
     }
 
 
-
+    function decryptMensagem() {
+        let mensagemEncrypted = document.getElementById('mensagemCriptografada');
+        let decrypted = document.getElementsByName('decrypted');
+    
+        
+    
+       console.log(decrypted)
+    
+    
+         mensagemEncrypted = mensagemEncrypted.value.toUpperCase().replace(/\s/g,'');
+    
+        for(let a = 0; a < mensagemEncrypted.length; a++) {
+            let letterCode;
+           let offset;
+    
+         
+    
+            letterCode = mensagemEncrypted.charCodeAt(a) - 65;
+             
+            offset = (letterCode - 3) % 26;
+    
+            offset += 65;
+    
+            decrypted.value += String.fromCharCode(offset);
+        }
+    }
 
     
